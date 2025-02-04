eb30f5f2912f409c08e9a7567676745d8156b5f7
alxroum <alxroum@gmail.com>
2025-02-04 17:40:04 -0500

    can now display individual numbers in each digit.

--- a.c
+++ a.c
@@ -7,41 +7,45 @@ int main(void) {
 	DCOCTL  = CALDCO_1MHZ;
 
     int i = 0;
+    int segments = 0b0000000; // this will change the segments that light up on the current "frame" or loop iteration
 
     while (1) { // Keep the program running
         //print_something();
 
-        int segments = 0b1111111; // this will change the segments that light up on the current "frame" or loop iteration
-
-        light_segments(segments);
+        //segments = 0b0000000;
 
         int digits = 0b0000;
         disable_digits();
+        disable_segments();
 
         if(i == 0) {
             digits |= BIT0;
+            segments = 0b0110000; // segments to be lit at digit 0
         }
         if(i == 1) {
             digits |= BIT1;
+            segments = 0b1011011;
         }
         if(i == 2) {
             digits |= BIT4;
+            segments = 0b1110100;
         }
         if(i == 3) {
             digits |= BIT7;
+            segments = 0b1111111;
         }
 
         enable_digits(digits);
 
-        //__delay_cycles(150000);
+        light_segments(segments);
+
+        __delay_cycles(1500);
 
         if(i >= 3) {
             i = 0;
         } else {
             i++;
         }
-
-
     }
 }
 
@@ -66,4 +70,14 @@ void light_segments(int s) {
 void disable_digits() {
     P3DIR |= BIT0 | BIT1 | BIT4 | BIT7;
     P3OUT |= BIT0 | BIT1 | BIT4 | BIT7;
+}
+
+void disable_segments() { // turn off any lingering bits so that they are not left displayed as segments
+
+    P2SEL &= ~(BIT6 | BIT7);
+    P2SEL2 &= ~(BIT6 | BIT7);
+
+    // BIT0 | BIT1 | BIT2 | BIT3 | BIT4 | BIT5 | BIT6
+    P2DIR &= ~(BIT0 | BIT1 | BIT2 | BIT3 | BIT4 | BIT5 | BIT6);
+    P2OUT &= ~(BIT0 | BIT1 | BIT2 | BIT3 | BIT4 | BIT5 | BIT6); // turns off bit segment c
 }
